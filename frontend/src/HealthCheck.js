ca9048b8bb297464348c2909d23067e56fb76f83
SoowanPark <jack6767@kau.kr>
2024-02-13 04:06:37 +0900

    test

---
+++ frontend/src/HealthCheck.js
@@ -0,0 +1,12 @@
+// HealthCheck.js
+import React from 'react';
+
+const HealthCheck = () => {
+    return (
+        <div>
+            <h1>I'm healthy</h1>
+        </div>
+    );
+};
+
+export default HealthCheck;
