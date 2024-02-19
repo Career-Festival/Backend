3a42ab44a9c81d2b64806867ebe8f484ab3736f8
Jeongyeon Park <githubb0208@gmail.com>
2024-02-20 01:57:01 +0900

    [Refactor] eventPage eventId, organizerId 반환 수정

--- src/main/java/careerfestival/career/register/dto/RegisterMainResponseDto.java
+++ src/main/java/careerfestival/career/register/dto/RegisterMainResponseDto.java
@@ -13,6 +13,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class RegisterMainResponseDto {
+
+    private Long eventId;
+
     private String eventMainFileUrl;
     private String eventName;
     private LocalDateTime recruitmentStart;
@@ -22,6 +25,7 @@ public class RegisterMainResponseDto {
 
     public static RegisterMainResponseDto fromEntity(Event event) {
         return RegisterMainResponseDto.builder()
+                .eventId(event.getId())
                 .eventMainFileUrl(event.getEventMainFileUrl())
                 .eventName(event.getEventName())
                 .recruitmentStart(event.getRecruitmentStart())
